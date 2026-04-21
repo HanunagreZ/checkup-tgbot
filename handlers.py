a57300dd1dcf817f5429ed5b2c55f57699267907
HanunagreZ <hanunagrez@yandex.ru>
2026-04-21 15:13:52 +0300

    render com settings

--- handlers.py
+++ handlers.py
@@ -3,7 +3,7 @@ from telegram.ext import CallbackContext
 from bot_config import FEELING_EMOJI, RESPONSE_COLORS
 from database import save_checkup, get_last_checkup, get_checkup_dates, get_user_checkups
 from keyboards import feeling_keyboard, yes_no_keyboard, observations_keyboard, main_menu_keyboard
-from calendar import build_calendar, build_day_detail
+from calendar_view import build_calendar, build_day_detail
 
 
 class CheckupState:
